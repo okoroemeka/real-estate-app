19a9b9bc084afc3c17994802b7c9688211ab4089
Emeka Okoro <emeka@Emekas-MacBook-Pro.local>
2020-06-06 19:48:47 +0100

    add recent properties

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HeroArea from './components/HeroArea';
 import OurProperty from './components/OurProperty';
 import OurOffer from './components/OurOffer';
+import RecentProperties from './components/RecentProperties';
 import './style/App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <HeroArea />
       <OurProperty />
       <OurOffer />
+      <RecentProperties />
     </div>
   );
 }
